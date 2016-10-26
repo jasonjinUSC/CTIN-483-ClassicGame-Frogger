6f3cc651ebc0e1f13b0bcd49089eb8aa7b78c689
Christopher Morales <ctmorale@usc.edu>
2016-10-26 09:49:17 -0700

    Adding PlayerJoystick Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+  
+
+    private void Update ()
+    {
+        transform.Translate(Vector3.forward * Input.GetAxis("RightJoystickY") * 3 * Time.deltaTime);
+        transform.Translate(Vector3.right * Input.GetAxis("RightJoystickX") * 3 * Time.deltaTime);
+
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * 3 * Time.deltaTime);
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * 3 * Time.deltaTime);
+
+    }
+
+}
+
+
